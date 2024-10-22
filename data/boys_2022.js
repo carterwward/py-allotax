56ec18a99579f4117b4a9a623f79cbfa1f1c8f30
Ashley Fehr <63805609+amafehr@users.noreply.github.com>
2024-10-22 09:04:19 -0400

    adds test files and folders for input and output data that the app will use and produce

---
+++ data/boys_2022.js
@@ -0,0 +1,7 @@
+export const data = [
+    {"types": "Liam", "counts": 20533, "totalunique": 14311, "probs": 0.0118},
+    {"types": "Noah", "counts": 18708, "totalunique": 14311, "probs": 0.0107},
+    {"types": "Oliver", "counts": 15141, "totalunique": 14311, "probs": 0.0087},
+    {"types": "James", "counts": 12089, "totalunique": 14311, "probs": 0.0069},
+    {"types": "Elijah", "counts": 12054, "totalunique": 14311, "probs": 0.0069}
+]
