844831a26efdb8844f6d337d4eac2f0e276f5f8a
Jonathan St-Onge <jonathanstonge7@gmail.com>
2024-10-30 10:48:34 -0400

    fix bug diamond plot

--- src/AllotaxChart.js
+++ src/AllotaxChart.js
@@ -11,26 +11,24 @@ async function createAllotaxChart(data_1, data_2, alpha, passed_svg) {
     
     // Create Data req for charts
     const dat = myDiamond(me, rtd);
-    const diamond_dat_raw = dat.counts;
-    const diamond_dat = diamond_dat_raw.filter(d => d.types !== "")
-
+    const diamond_dat = dat.counts;
     const barData = wordShift_dat(me, dat).slice(0, 30);
 
     max_shift = d3.max(barData, d => Math.abs(d.metric))
 
     // Plot
     DiamondChart(diamond_dat, alpha, passed_svg.diamond_svg);
-    
+
     WordShiftChart(barData, {  
-      x: d => d.metric,
-      y: d => d.type,
-      xFormat: "%",
-      xDomain: [-max_shift*1.5, max_shift*1.5], // [xmin, xmax]
-      width: 300,
-      yPadding: 0.2,
-      height: 740,
-      xLabel: "← System 1 · Divergence contribution · System 2 →",
-      colors: ["lightgrey", "lightblue"] }, passed_svg.wordshift_svg);
+        x: d => d.metric,
+        y: d => d.type,
+        xFormat: "%",
+        xDomain: [-max_shift*1.5, max_shift*1.5], // [xmin, xmax]
+        width: 300,
+        yPadding: 0.2,
+        height: 680,
+        xLabel: "← System 1 · Divergence contribution · System 2 →",
+        colors: ["lightgrey", "lightblue"] }, passed_svg.wordshift_svg)
     
     BalanceChart(balanceDat(data_1, data_2), {
         x: d => d.frequency,
