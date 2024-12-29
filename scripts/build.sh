e0299dff261495a92e5babf48f67f861b9f133da
carterwward <Carter.Ward@uvm.edu>
2024-12-29 15:53:34 -0500

    add package.json and lock to src, update build script to install nvm. If nvm is already installed, no problem

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install --lts
+nvm use --lts
+# Build package
 docker build --no-cache -t allotax .
 docker rm allotax
 docker run --name allotax allotax
